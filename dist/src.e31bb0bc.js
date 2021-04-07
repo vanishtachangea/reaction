da3fab9c5be848d8ac9ffd396a4481a11b3d50d6
vanishta <vanishta.changea@gmail.com>
2021-04-07 17:08:35 +0200

    modify

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -33406,7 +33406,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34181" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "31125" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
